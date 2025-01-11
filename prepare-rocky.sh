baeb8f143260edc3376a9c98b317be5a5e7098ef
Weasel <Weasel@WeaselsLair.com>
2025-01-10 19:15:00 -0800

    More updates for testing Rocky/Red-Hat compatibility.

--- prepare-rocky.sh
+++ prepare-rocky.sh
@@ -127,7 +127,7 @@ if [[ $OS_DETECTION == *".el"* ]]; then
        COMPATIBLE_OS=true
 
         subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms;
-        dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm;
+        dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm;
 
 fi;
 if ! [[ $COMPATIBLE_OS == true ]]; then
